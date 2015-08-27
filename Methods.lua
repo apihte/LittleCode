acced4d4f340a9f7381627638b67f92a7e17541e
nomeats <nomeatscn@gmail.com>
2015-08-27 13:07:37 +0800

    Add Sort Methods
    QuickSort and BubbleSort

--- Methods.lua
+++ Methods.lua
@@ -59,3 +59,55 @@ local function Fibonacci(uNumSum)
 
 	FibonacciFunc(uNumSum)
 end
+
+---------------------------------------------->>>
+--QuickSort
+function QuickSort(tSrcTbl)
+	if #tSrcTbl == 0 then
+		return
+	end
+	local QuickSortFunc
+	QuickSortFunc = function(tSrcTbl, uLeft, uRight)
+		if uLeft < uRight then
+			local i = uLeft
+			local j = uRight
+			local uFlag = tSrcTbl[uLeft]
+			while i < j do
+				while i < j and tSrcTbl[j] >= uFlag do
+					j = j - 1
+				end
+				while i < j and tSrcTbl[i] <= uFlag do
+					i = i + 1
+				end
+				if i < j then
+					local uTmp = tSrcTbl[i]
+					tSrcTbl[i] = tSrcTbl[j]
+					tSrcTbl[j] = uTmp
+				end
+			end
+			tSrcTbl[uLeft] = tSrcTbl[i]
+			tSrcTbl[i] = uFlag
+			QuickSortFunc(tSrcTbl, uLeft, i - 1)
+			QuickSortFunc(tSrcTbl, i + 1, uRight)
+		end
+	end
+
+	QuickSortFunc(tSrcTbl, 1, #tSrcTbl)
+end
+
+---------------------------------------------->>>
+--BubbleSort
+function BubbleSort(tSrcTbl)
+	if #tSrcTbl == 0 then
+		return
+	end
+	for i = 1, #tSrcTbl do
+		for j = i + 1, #tSrcTbl do
+			if tSrcTbl[i] > tSrcTbl[j] then
+				local uTmp = tSrcTbl[i]
+				tSrcTbl[i] = tSrcTbl[j]
+				tSrcTbl[j] = uTmp
+			end
+		end
+	end
+end
